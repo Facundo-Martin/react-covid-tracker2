88ca2a4145c59dcc762ad0eeb67c484fcf071088
Facundo Martin <facundomartin98@gmail.com>
2022-03-18 13:44:20 -0400

    final commit

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import "./Article.css";
+import { GrFormNextLink } from "react-icons/gr";
 
 const Article = ({ image, title, author, date, summary, link }) => {
-  const summaryLength = summary.length;
-  console.log(summaryLength);
   return (
     <div className="app__article">
       <div className="app__article-image">
@@ -14,22 +13,25 @@ const Article = ({ image, title, author, date, summary, link }) => {
 
       <div className="app__article-content">
         <div className="app__article-heading">
-          <h4>{title}</h4>
+          <h4>{title.length <= 60 ? title : `${title.slice(0, 50)}...`}</h4>
         </div>
         <div className="app__article-subheading">
-          <p>{author ? author : "Random Author"}</p>
+          <p>{author?.length <= 70 ? author : "Unknown Author"}</p>
           <div>
             <i>{date}</i>
           </div>
         </div>
         <div className="app__article-summary">
           <div className="app__article-summary-content">
-            {/* {summary.Length <= 800 ? summary : "PUTO EL QUE LEE"} */}
-            {summary}
+            {summary.length <= 305 ? summary : `${summary.slice(0, 310)}...`}
           </div>
-          ...
         </div>
-        <a href={link}>Read More</a>
+        <div className="app__article-link">
+          <a href={link} target="_blank">
+            Read More
+          </a>
+          <GrFormNextLink fontSize={20} />
+        </div>
       </div>
     </div>
   );
